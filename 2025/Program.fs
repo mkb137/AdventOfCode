a8c82dfb3b72631e005123f135a148bc04ea42e9
Mike Bennett <mike.bennett@peloton.com>
2025-12-05 18:36:17 -0700

    Initial commit

---
+++ 2025/Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Microsoft.Extensions.Configuration
+open Serilog
+
+let configuration =
+        ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build()
+Log.Logger <- LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger()
+
+Log.Debug "Start"
